5a9e137b8a549b924ed4dd290c41f9c26ba8454b
Preesha Shankar <preesha.shankar@ibm.com>
2022-08-08 13:25:45 -0500

    Customizing the mq-qm01 event trigger
    Signed-off-by: Preesha Shankar <preesha.shankar@ibm.com>

--- mq/environments/ci/eventlisteners/cntk-event-listener.sh
+++ mq/environments/ci/eventlisteners/cntk-event-listener.sh
@@ -10,7 +10,7 @@ GIT_BRANCH_QM1=${GIT_BRANCH_QM1:-master}
 GIT_BRANCH_SPRING=${GIT_BRANCH_SPRING:-master}
 
 # Create Kubernetes Secret yaml
-( echo "cat <<EOF" ; cat cntk-event-listener.yaml_template ; echo EOF ) | \
+( echo "cat <<EOF" ; cat cntk-event-listener.yaml_template) | \
 GIT_ORG=${GIT_ORG} \
 GIT_BRANCH_QM1=${GIT_BRANCH_QM1:-master} \
 GIT_BRANCH_SPRING=${GIT_BRANCH_SPRING:-master} \
